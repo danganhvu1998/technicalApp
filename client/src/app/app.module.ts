e876b6ff3a64d52b3ef5b3be50ecfa3cbeb0e6df
Dang Anh Vu <danganhvu1998@gmail.com>
2018-06-19 13:27:49 +0900

    Migrate Laravel, Testing @angular/common/http - Something wrong

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login'
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -15,18 +19,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    LoginPage,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    LoginPage,
   ],
   providers: [
     StatusBar,
---
+++ client/src/pages/login/login.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(
+  	public navCtrl: NavController,
+  	public navParams: NavParams,
+    public http: HttpClient,
+    public store: Storage) {
+  }
+
+  async ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  async testing(){
+    //let response = await this.http.post(url, DATA, options).toPromise()
+    let headers = new HttpHeaders({ 'Content-Type': 'x-www-form-urlencoded' });
+    let options = { headers: headers };
+    let url = "http://localhost:8000/api/data";
+    //let response = this.http.get("url");
+    //console.log(response);
+    //return 0;
+    let DATA = "data=502f13a6a4bd7bdd30b2d78dd0a05677c098233e";
+    let data = { data : '502f13a6a4bd7bdd30b2d78dd0a05677c098233e' }
+    this.http.post(url, DATA, options)
+      .toPromise().then((response) => {
+        console.log('API Response : ', response);
+      })
+      .catch((error) => { 
+        console.error('API Error : ', error.status);
+      });
+  }
+
+  async testing2(){
+    let response = this.http.get("http://localhost:8000/api/data");
+    console.log(response);
+  }
+
+  postAjax(){
+    let DATA = "data=this_is_data_data";
+    let url = "http://localhost:8000/api/data";
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.onreadystatechange = function() {
+      console.log("Current status:", xhr.readyState, xhr.status);
+      if (xhr.readyState>3 && xhr.status==200) {
+        console.log("result:",xhr.responseText);
+      }
+    };
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    //xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(DATA);
+    return xhr;
+  }
+}
